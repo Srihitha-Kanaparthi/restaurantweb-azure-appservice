d87fe00f67dcad67fb9cc34cfcdbe155000c31a5
Srihitha-Kanaparthi <srihithakanaparthi@gmail.com>
2025-04-14 20:58:44 +0000

    Configure Azure database and cache connectons

--- azureproject/production.py
+++ azureproject/production.py
@@ -33,10 +33,10 @@ conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.s
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': conn_str_params['AZURE_POSTGRESQL_NAME'],
+        'HOST': conn_str_params['AZURE_POSTGRESQL_HOST'],
+        'USER': conn_str_params['AZURE_POSTGRESQL_USER'],
+        'PASSWORD': conn_str_params['AZURE_POSTGRESQL_PASSWORD'],
     }
 }
 
